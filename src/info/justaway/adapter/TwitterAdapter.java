c1079bc18528b545f73effa919aa60379a5868d8
Shinichiro Aska <s.aska.org@gmail.com>
2013-10-04 02:19:10 +0900

    Guy to use picasso.

--- src/info/justaway/adapter/TwitterAdapter.java
+++ src/info/justaway/adapter/TwitterAdapter.java
@@ -6,32 +6,24 @@ import info.justaway.ProfileActivity;
 import info.justaway.R;
 import info.justaway.model.Row;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Locale;
+
+import com.squareup.picasso.Picasso;
 
 import twitter4j.DirectMessage;
 import twitter4j.MediaEntity;
 import twitter4j.Status;
 import twitter4j.User;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.support.v4.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class TwitterAdapter extends ArrayAdapter<Row> {
@@ -39,7 +31,6 @@ public class TwitterAdapter extends ArrayAdapter<Row> {
     private ArrayList<Row> statuses = new ArrayList<Row>();
     private LayoutInflater inflater;
     private int layout;
-    private LruCache<String, Bitmap> mMemoryCache;
     private static int limit = 500;
 
     public TwitterAdapter(Context context, int textViewResourceId) {
@@ -47,24 +38,6 @@ public class TwitterAdapter extends ArrayAdapter<Row> {
         this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.context = context;
         this.layout = textViewResourceId;
-
-        // Get max available VM memory, exceeding this amount will throw an
-        // OutOfMemory exception. Stored in kilobytes as LruCache takes an
-        // int in its constructor.
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-
-        // Use 1/8th of the available memory for this memory cache.
-        final int cacheSize = maxMemory / 2;
-
-        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
-            @SuppressLint("NewApi")
-            @Override
-            protected int sizeOf(String key, Bitmap bitmap) {
-                // The cache size will be measured in kilobytes rather than
-                // number of items.
-                return bitmap.getByteCount() / 1024;
-            }
-        };
     }
 
     @Override
@@ -176,8 +149,7 @@ public class TwitterAdapter extends ArrayAdapter<Row> {
         view.findViewById(R.id.retweet).setVisibility(View.GONE);
         view.findViewById(R.id.images).setVisibility(View.GONE);
         ImageView icon = (ImageView) view.findViewById(R.id.icon);
-        ProgressBar wait = (ProgressBar) view.findViewById(R.id.WaitBar);
-        renderIcon(wait, icon, message.getSender().getBiggerProfileImageURL());
+        Picasso.with(context).load(message.getSender().getBiggerProfileImageURL()).into(icon);
     }
 
     private void renderStatus(View view, final Status status, Status retweet, User favorite) {
@@ -198,8 +170,7 @@ public class TwitterAdapter extends ArrayAdapter<Row> {
             ((TextView) view.findViewById(R.id.retweet_by)).setText("favorited by "
                     + favorite.getScreenName() + "(" + favorite.getName() + ")");
             ImageView icon = (ImageView) view.findViewById(R.id.retweet_icon);
-            ProgressBar wait = (ProgressBar) view.findViewById(R.id.retweet_wait);
-            renderIcon(wait, icon, favorite.getMiniProfileImageURL());
+            Picasso.with(context).load(favorite.getMiniProfileImageURL()).into(icon);
             view.findViewById(R.id.retweet).setVisibility(View.VISIBLE);
         }
         // RTの場合
@@ -208,16 +179,14 @@ public class TwitterAdapter extends ArrayAdapter<Row> {
                     + retweet.getUser().getScreenName() + "(" + retweet.getUser().getName()
                     + ") and " + String.valueOf(status.getRetweetCount()) + " others");
             ImageView icon = (ImageView) view.findViewById(R.id.retweet_icon);
-            ProgressBar wait = (ProgressBar) view.findViewById(R.id.retweet_wait);
-            renderIcon(wait, icon, retweet.getUser().getMiniProfileImageURL());
+            Picasso.with(context).load(retweet.getUser().getMiniProfileImageURL()).into(icon);
             view.findViewById(R.id.retweet).setVisibility(View.VISIBLE);
         } else {
             view.findViewById(R.id.retweet).setVisibility(View.GONE);
         }
 
         ImageView icon = (ImageView) view.findViewById(R.id.icon);
-        ProgressBar wait = (ProgressBar) view.findViewById(R.id.WaitBar);
-        renderIcon(wait, icon, status.getUser().getBiggerProfileImageURL());
+        Picasso.with(context).load(status.getUser().getBiggerProfileImageURL()).into(icon);
         icon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -238,7 +207,7 @@ public class TwitterAdapter extends ArrayAdapter<Row> {
                 image.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 images.addView(image, new LinearLayout.LayoutParams(
                         ViewGroup.LayoutParams.WRAP_CONTENT, 120));
-                renderIcon(null, image, url.getMediaURL());
+                Picasso.with(context).load(url.getMediaURL()).into(image);
                 // 画像タップで拡大表示（ピンチイン・ピンチアウトいつかちゃんとやる）
                 image.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -255,89 +224,12 @@ public class TwitterAdapter extends ArrayAdapter<Row> {
         }
     }
 
-    // アイコンを読み込む
-    private void renderIcon(ProgressBar wait, ImageView icon, String url) {
-        if (wait != null) {
-            wait.setVisibility(View.VISIBLE);
-        }
-        icon.setVisibility(View.GONE);
-        String tag = (String) icon.getTag();
-        if (tag != null && tag == url) {
-        } else {
-            icon.setTag(url);
-            Bitmap image = mMemoryCache.get(url);
-            if (image == null) {
-                ImageGetTask task = new ImageGetTask(icon, wait);
-                task.execute(url);
-            } else {
-                icon.setImageBitmap(image);
-                icon.setVisibility(View.VISIBLE);
-                if (wait != null) {
-                    wait.setVisibility(View.GONE);
-                }
-            }
-        }
-    }
-
     private String getClientName(String source) {
         String[] tokens = source.split("[<>]");
         if (tokens.length > 1) {
             return tokens[2];
         } else {
             return tokens[0];
-        }
-    }
-
-    class ImageGetTask extends AsyncTask<String, Void, Bitmap> {
-        private ImageView image;
-        private String tag;
-        private ProgressBar bar;
-
-        public ImageGetTask(ImageView image, ProgressBar bar) {
-            // 対象の項目を保持しておく
-            this.image = image;
-            this.bar = bar;
-            this.tag = image.getTag().toString();
-        }
-
-        @Override
-        protected Bitmap doInBackground(String... params) {
-            // ここでHttp経由で画像を取得します。取得後Bitmapで返します。
-            synchronized (context) {
-                try {
-                    URL imageUrl = new URL(params[0]);
-                    InputStream imageIs;
-                    imageIs = imageUrl.openStream();
-                    Bitmap bitmap = BitmapFactory.decodeStream(imageIs);
-                    return bitmap;
-                } catch (MalformedURLException e) {
-                    return null;
-                } catch (IOException e) {
-                    return null;
-                }
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            // Tagが同じものか確認して、同じであれば画像を設定する
-            // （Tagの設定をしないと別の行に画像が表示されてしまう）
-            if (tag.equals(image.getTag())) {
-                if (bitmap != null) {
-                    // 画像の設定
-                    mMemoryCache.put(tag, bitmap);
-                    image.setImageBitmap(bitmap);
-                } else {
-                    // エラーの場合は×印を表示
-                    // image.setImageDrawable(context.getResources().getDrawable(
-                    // R.drawable.x));
-                }
-                // プログレスバーを隠し、取得した画像を表示
-                image.setVisibility(View.VISIBLE);
-                if (bar != null) {
-                    bar.setVisibility(View.GONE);
-                }
-            }
         }
     }
 }
