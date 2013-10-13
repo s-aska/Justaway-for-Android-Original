d68231e7ab44b9cadd7eade22c64b6e049fbfd62
teshi04 <teshi04@gmail.com>
2013-10-13 17:59:39 +0900

    Guy who can see that you are followers.

---
+++ src/info/justaway/model/Profile.java
@@ -0,0 +1,22 @@
+package info.justaway.model;
+
+import twitter4j.Relationship;
+import twitter4j.User;
+
+public class Profile {
+
+    private User user;
+    private Relationship relationship;
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public Relationship getRelationship() {
+        return relationship;
+    }
+    public void setRelationship(Relationship relationship) {
+        this.relationship = relationship;
+    }
+}
