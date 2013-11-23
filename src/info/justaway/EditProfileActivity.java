a0bd081331ea56af4c46e3d14bc92554eb02563f
teshi04 <teshi04@gmail.com>
2013-11-23 16:47:31 +0900

    Guy you want to edit profile.

--- src/info/justaway/EditProfileActivity.java
+++ src/info/justaway/EditProfileActivity.java
@@ -2,8 +2,12 @@ package info.justaway;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import twitter4j.Twitter;
 import twitter4j.User;
@@ -14,6 +18,11 @@ public class EditProfileActivity extends Activity {
     private Twitter twitter;
     private User user;
 
+    private EditText name;
+    private EditText location;
+    private EditText url;
+    private EditText description;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +32,53 @@ public class EditProfileActivity extends Activity {
         JustawayApplication application = JustawayApplication.getApplication();
         twitter = application.getTwitter();
 
-        user = application.getUser();
-        ((EditText) findViewById(R.id.name)).setText(user.getName());
-        ((EditText) findViewById(R.id.location)).setText(user.getLocation());
-        ((EditText) findViewById(R.id.webSite)).setText(user.getURL());
-        ((EditText) findViewById(R.id.bio)).setText(user.getDescription());
+        Intent intent = getIntent();
+        user = (User) intent.getSerializableExtra("user");
+
+        //user = application.getUser();
+        name = ((EditText) findViewById(R.id.name));
+        name.setText(user.getName());
+        location = ((EditText) findViewById(R.id.location));
+        location.setText(user.getLocation());
+        url = ((EditText) findViewById(R.id.webSite));
+        url.setText(user.getURL());
+        description = ((EditText) findViewById(R.id.bio));
+        description.setText(user.getDescription());
+
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new UpdateProfileTask().execute();
+            }
+        });
     }
 
+    private class UpdateProfileTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                twitter.updateProfile(name.getText().toString(), location.getText().toString(),
+                        url.getText().toString(), description.getText().toString());
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            // dismissProgressDialog();
+            if (success) {
+                showToast("プロフィールを保存しました");
+                finish();
+            } else {
+                showToast("プロフィールの保存に失敗しました");
+            }
+        }
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
 }
