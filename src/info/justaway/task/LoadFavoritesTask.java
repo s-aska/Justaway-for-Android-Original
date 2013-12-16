3098c5c98b808d75c71d55216da0bc3a6fec3842
Shinichiro Aska <s.aska.org@gmail.com>
2013-12-17 04:58:28 +0900

    Guy to remove the favorites loading process at startup.

---
+++ src/info/justaway/task/LoadFavoritesTask.java
@@ -0,0 +1,24 @@
+package info.justaway.task;
+
+import android.os.AsyncTask;
+
+import info.justaway.JustawayApplication;
+import twitter4j.ResponseList;
+
+public class LoadFavoritesTask extends AsyncTask<Long, Void, Boolean> {
+
+    @Override
+    protected Boolean doInBackground(Long... params) {
+        try {
+            JustawayApplication application = JustawayApplication.getApplication();
+            ResponseList<twitter4j.Status> favorites = application.getTwitter().getFavorites(application.getUserId());
+            for (twitter4j.Status status : favorites) {
+                application.setFav(status.getId());
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- src/info/justaway/task/VerifyCredentialsLoader.java
+++ src/info/justaway/task/VerifyCredentialsLoader.java
@@ -1,8 +1,6 @@
 package info.justaway.task;
 
 import info.justaway.JustawayApplication;
-import twitter4j.ResponseList;
-import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.User;
@@ -23,10 +21,6 @@ public class VerifyCredentialsLoader extends AbstractAsyncTaskLoader<User> {
             User user = twitter.verifyCredentials();
             application.setUserId(user.getId());
             application.setScreenName(user.getScreenName());
-//            ResponseList<Status> favorites = twitter.getFavorites(user.getId());
-//            for (Status status : favorites) {
-//                application.setFav(status.getId());
-//            }
             return user;
         } catch (TwitterException e) {
             e.printStackTrace();
