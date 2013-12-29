7f8284a194d46107431fc4d7734ea67eda870643
teshi04 <teshi04@gmail.com>
2013-12-29 21:15:55 +0900

    Guy to change the button text in the follow / unfollow in the profile screen.

--- src/info/justaway/task/DestroyFriendshipTask.java
+++ src/info/justaway/task/DestroyFriendshipTask.java
@@ -3,7 +3,6 @@ package info.justaway.task;
 import android.os.AsyncTask;
 
 import info.justaway.JustawayApplication;
-import info.justaway.R;
 
 public class DestroyFriendshipTask extends AsyncTask<Long, Void, Boolean> {
 
@@ -15,13 +14,6 @@ public class DestroyFriendshipTask extends AsyncTask<Long, Void, Boolean> {
         } catch (Exception e) {
             e.printStackTrace();
             return false;
-        }
-    }
-
-    @Override
-    protected void onPostExecute(Boolean success) {
-        if (success) {
-            JustawayApplication.showToast(R.string.toast_destroy_friendship_success);
         }
     }
 }
