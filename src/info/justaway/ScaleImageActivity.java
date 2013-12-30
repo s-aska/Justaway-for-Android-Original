cf766eec12cc11ad3c0106ad45f574e1f954edaf
Shinichiro Aska <s.aska.org@gmail.com>
2013-12-30 17:36:45 +0900

    Guy to remove import Log

--- src/info/justaway/ScaleImageActivity.java
+++ src/info/justaway/ScaleImageActivity.java
@@ -10,7 +10,6 @@ import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
