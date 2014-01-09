f368ca6aa39a1a27f2c3a511d3952fddff74a870
teshi04 <teshi04@gmail.com>
2014-01-10 01:22:14 +0900

    Guy to tweet displayed in the user list screen.

---
+++ src/info/justaway/fragment/list/UserListStatusesFragment.java
@@ -0,0 +1,120 @@
+package info.justaway.fragment.list;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import info.justaway.BaseActivity;
+import info.justaway.JustawayApplication;
+import info.justaway.R;
+import info.justaway.adapter.TwitterAdapter;
+import info.justaway.model.Row;
+import twitter4j.Paging;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+
+public class UserListStatusesFragment extends Fragment {
+    private TwitterAdapter mAdapter;
+    private ListView mListView;
+    private int mListId;
+    private ProgressBar mFooter;
+    private Boolean mAutoLoader = false;
+    private int mPage = 1;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.list, container, false);
+        if (v == null) {
+            return null;
+        }
+
+        mListId = getArguments().getInt("listId");
+
+        // リストビューの設定
+        mListView = (ListView) v.findViewById(R.id.list_view);
+        mListView.setVisibility(View.GONE);
+
+        // コンテキストメニューを使える様にする為の指定、但しデフォルトではロングタップで開く
+        registerForContextMenu(mListView);
+
+        mFooter = (ProgressBar) v.findViewById(R.id.guruguru);
+
+        mAdapter = new TwitterAdapter(getActivity(), R.layout.row_tweet);
+        mListView.setAdapter(mAdapter);
+
+        // シングルタップでコンテキストメニューを開くための指定
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.showContextMenu();
+            }
+        });
+
+        new UserListTask().execute(mListId);
+
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                // 最後までスクロールされたかどうかの判定
+                if (totalItemCount == firstVisibleItem + visibleItemCount) {
+                    additionalReading();
+                }
+            }
+        });
+        return v;
+    }
+
+    private void additionalReading() {
+        if (!mAutoLoader) {
+            return;
+        }
+        mFooter.setVisibility(View.VISIBLE);
+        mAutoLoader = false;
+        new UserListTask().execute(mListId);
+    }
+
+
+    private class UserListTask extends AsyncTask<Integer, Void, ResponseList<Status>> {
+        @Override
+        protected ResponseList<twitter4j.Status> doInBackground(Integer... params) {
+            try {
+                ResponseList<twitter4j.Status> statuses = JustawayApplication.getApplication().getTwitter().getUserListStatuses(params[0], new Paging(mPage));
+                return statuses;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ResponseList<twitter4j.Status> statuses) {
+            mFooter.setVisibility(View.GONE);
+
+            if (statuses == null || statuses.size() == 0) {
+                return;
+            }
+
+            for (twitter4j.Status status : statuses) {
+                mAdapter.add(Row.newStatus(status));
+            }
+            mPage++;
+            mAutoLoader = true;
+            mListView.setVisibility(View.VISIBLE);
+        }
+    }
+
+}
