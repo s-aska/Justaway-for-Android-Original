b98e0a701e4166bf41acd6064b12d68d6a235175
Shinichiro Aska <s.aska.org@gmail.com>
2014-02-04 00:50:48 +0900

    Guy to added already favorites/create and  favorites/destroy message.

--- src/info/justaway/task/FavoriteTask.java
+++ src/info/justaway/task/FavoriteTask.java
@@ -4,26 +4,31 @@ import android.os.AsyncTask;
 
 import info.justaway.JustawayApplication;
 import info.justaway.R;
+import twitter4j.TwitterException;
 
-public class FavoriteTask extends AsyncTask<Long, Void, Boolean> {
+public class FavoriteTask extends AsyncTask<Long, Void, TwitterException> {
 
     @Override
-    protected Boolean doInBackground(Long... params) {
+    protected TwitterException doInBackground(Long... params) {
+        JustawayApplication application = JustawayApplication.getApplication();
         try {
-            JustawayApplication application = JustawayApplication.getApplication();
             application.getTwitter().createFavorite(params[0]);
-            application.setFav(params[0]);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+        } catch (TwitterException e) {
+            if (e.getErrorCode() == 139) {
+                application.setFav(params[0]);
+            }
+            return e;
         }
+        application.setFav(params[0]);
+        return null;
     }
 
     @Override
-    protected void onPostExecute(Boolean success) {
-        if (success) {
+    protected void onPostExecute(TwitterException e) {
+        if (e == null) {
             JustawayApplication.showToast(R.string.toast_favorite_success);
+        } else if (e.getErrorCode() == 139) {
+            JustawayApplication.showToast(R.string.toast_favorite_already);
         } else {
             JustawayApplication.showToast(R.string.toast_favorite_failure);
         }
