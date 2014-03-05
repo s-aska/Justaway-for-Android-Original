3b2ed7533675d91383f85f536ff87848cc0ac3d1
Shinichiro Aska <s.aska.org@gmail.com>
2014-03-05 14:46:28 +0900

    Guy to fixed #88

--- src/info/justaway/SettingsActivity.java
+++ src/info/justaway/SettingsActivity.java
@@ -1,11 +1,13 @@
 package info.justaway;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.view.MenuItem;
 
 public class SettingsActivity extends Activity {
 
@@ -15,6 +17,22 @@ public class SettingsActivity extends Activity {
         getWindow().setBackgroundDrawableResource(R.color.background);
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new SettingsFragment()).commit();
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
     }
 
     public static class SettingsFragment extends PreferenceFragment {
