7e26579e9313e4fc2ec84eae0de58db7255120bc
teshi04 <teshi04@gmail.com>
2014-05-04 19:14:20 +0900

    JustawayButtonを作成

---
+++ Justaway/src/main/java/info/justaway/widget/JustawayButton.java
@@ -0,0 +1,44 @@
+package info.justaway.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.Button;
+
+import info.justaway.JustawayApplication;
+import info.justaway.R;
+
+public class JustawayButton extends Button {
+
+    public JustawayButton(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public JustawayButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public JustawayButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        setTypeface(JustawayApplication.getFontello());
+        setTextSize(22);
+
+        // テーマによってボタンの色を変える
+        TypedValue outValueBackground = new TypedValue();
+        TypedValue outValueTextColor = new TypedValue();
+        Resources.Theme theme = context.getTheme();
+        if (theme != null) {
+            theme.resolveAttribute(R.attr.button_stateful, outValueBackground, true);
+            theme.resolveAttribute(R.attr.menu_text_color, outValueTextColor, true);
+        }
+        setTextColor(outValueTextColor.data);
+        setBackgroundResource(outValueBackground.resourceId);
+    }
+}
