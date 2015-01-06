161f77c20dade67d40859f53912e62a89c862fc9
Shinichiro Aska <s.aska.org@gmail.com>
2015-01-06 22:36:19 +0900

    画像に黒い縁が付く件

--- Justaway/src/main/java/info/justaway/ScaleImageActivity.java
+++ Justaway/src/main/java/info/justaway/ScaleImageActivity.java
@@ -1,23 +1,28 @@
 package info.justaway;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,6 +39,7 @@ import info.justaway.widget.ScaleImageView;
 public class ScaleImageActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<String> {
 
     private ScaleImageView mImageView;
+    private String mUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,27 +54,26 @@ public class ScaleImageActivity extends FragmentActivity implements LoaderManage
 
         // インテント経由での起動をサポート
         Intent intent = getIntent();
-        String url;
         if (Intent.ACTION_VIEW.equals(intent.getAction())) {
             Uri data = intent.getData();
             if (data == null) {
                 return;
             }
-            url = data.toString();
+            mUrl = data.toString();
         } else {
             Bundle args = intent.getExtras();
             if (args == null) {
                 return;
             }
-            url = args.getString("url");
+            mUrl = args.getString("url");
         }
 
-        if (url == null) {
+        if (mUrl == null) {
             return;
         }
 
         Pattern pattern = Pattern.compile("https?://twitter\\.com/\\w+/status/(\\d+)/photo/(\\d+)/?.*");
-        Matcher matcher = pattern.matcher(url);
+        Matcher matcher = pattern.matcher(mUrl);
         if (matcher.find()) {
             Bundle args = new Bundle(1);
             args.putLong("statusId", Long.valueOf(matcher.group(1)));
@@ -77,7 +82,7 @@ public class ScaleImageActivity extends FragmentActivity implements LoaderManage
             return;
         }
 
-        ImageLoader.getInstance().displayImage(url, mImageView);
+        ImageLoader.getInstance().displayImage(mUrl, mImageView);
     }
 
     @Override
@@ -107,27 +112,42 @@ public class ScaleImageActivity extends FragmentActivity implements LoaderManage
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.save) {
-            mImageView.setDrawingCacheEnabled(false);
-            mImageView.setDrawingCacheEnabled(true);
-            Bitmap bitmap = mImageView.getDrawingCache(false);
-            if (bitmap == null) {
-                return false;
-            }
-            File root = new File(Environment.getExternalStorageDirectory(), "/Download/");
-            try {
-                File file = new File(root, new Date().getTime() + ".jpg");
-                FileOutputStream fos = new FileOutputStream(file);
-                bitmap.compress(CompressFormat.JPEG, 100, fos);
-                fos.close();
-                // ギャラリーに登録
-                String[] paths = {file.getPath()};
-                String[] types = {"image/jpeg"};
-                MediaScannerConnection.scanFile(getApplicationContext(), paths, types, null);
-                MessageUtil.showToast(R.string.toast_save_image_success);
-                finish();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+                @Override
+                protected Void doInBackground(Void... params) {
+                    int count;
+                    try {
+                        URL url = new URL(mUrl);
+                        URLConnection connection = url.openConnection();
+                        connection.connect();
+                        InputStream input = new BufferedInputStream(url.openStream(), 10 * 1024);
+                        File root = new File(Environment.getExternalStorageDirectory(), "/Download/");
+                        File file = new File(root, new Date().getTime() + ".jpg");
+                        OutputStream output = new FileOutputStream(file);
+                        byte data[] = new byte[1024];
+                        while ((count = input.read(data)) != -1) {
+                            output.write(data, 0, count);
+                        }
+                        output.flush();
+                        output.close();
+                        input.close();
+                        String[] paths = {file.getPath()};
+                        String[] types = {"image/jpeg"};
+                        MediaScannerConnection.scanFile(getApplicationContext(), paths, types, null);
+                        MessageUtil.showToast(R.string.toast_save_image_success);
+                        finish();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void result){
+                    MessageUtil.showToast(R.string.toast_save_image_success);
+                }
+            };
+            task.execute();
         }
         return true;
     }
